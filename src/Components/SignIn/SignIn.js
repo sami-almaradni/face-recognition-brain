7a51f1ebf38488228bb00010ca768b0a2de5291d
samialmaradni97 <sami.almaradni97@gmail.com>
2022-09-19 21:09:49 +0200

    add deployed link

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignin = () => {
-        fetch("http://localhost:3000/signin", {
+        fetch("https://almaradni-smart-brain-api.herokuapp.com/signin", {
             method: "post",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
